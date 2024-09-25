6f37392ab80c66a65135f096a8b9531f7055eb9c
Sarang Nirwan <74551805+sarang2551@users.noreply.github.com>
2024-09-25 12:04:35 +0800

    Rebased

--- app/src/main/java/com/example/fittr_app/MainActivity.kt
+++ app/src/main/java/com/example/fittr_app/MainActivity.kt
@@ -4,26 +4,21 @@ import android.os.Bundle
 import android.view.Window
 import android.view.WindowManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.fittr_app.databinding.ActivityMainBinding
 import androidx.activity.viewModels
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
-import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.fittr_app.media_pipe.CameraFragment
 import com.example.fittr_app.media_pipe.GalleryFragment
 import kotlinx.coroutines.launch
-import android.widget.TextView
 import android.graphics.LinearGradient
 import android.graphics.Shader
-import androidx.lifecycle.ViewModelProvider
+
 
 
 class MainActivity : AppCompatActivity() {
